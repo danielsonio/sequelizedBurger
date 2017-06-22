ff2dc91f7aadea7c990a0a60f0a02eadf42ba49c
Daniel Walden <Ariel@ACA80185.ipt.aol.com>
2017-06-22 11:47:34 -0500

    bugs

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
 
-var PORT = process.env.PORT || 3000;
-
 var app = express();
 
 var db = require('./models')
