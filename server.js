d7bffe162da63fd4067e5a22ca72cee272dc0686
Daniel Walden <Ariel@ACA80185.ipt.aol.com>
2017-06-22 11:51:25 -0500

    ..

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.static(__dirname + "/public"));
 require("./controllers/burgers_controller.js")(app);
 
 db.sequelize.sync().then(function() {
-  app.listen(process.env.PORT, function() {
+  app.listen(PORT, function() {
     console.log("listening on port %s", PORT);
   });
 });
